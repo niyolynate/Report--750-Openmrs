45dcab17a5e90f107cf3b49f658f921e04cd502c
niyolynate <98440370+niyolynate@users.noreply.github.com>
2022-03-20 06:19:47 +0300

    Update hash.java

---
+++ boot/hash.java
@@ -0,0 +1,19 @@
+com.niyolynatecode
+
+//Java Program to demonstate the use of GitHub.  
+
+public class Gitlesson {
+
+private String = "Name"
+private String = ID
+private int    =  "age"
+private  charAt = "0/oi77"
+
+    //defining an 'age' variable  
+    int age=20;  
+    //checking the age  
+    if(age>18){  
+        System.out.print("Age is greater than 18");  
+    }  
+}  
+}  
